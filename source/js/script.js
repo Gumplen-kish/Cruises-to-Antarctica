4cb9c6fd37fef1b7c9fcf14b98018f52d4349499
Gumplen-kish <gumplen117@gmail.com>
2020-10-04 02:40:32 +0300

    Пробую делать блоки резиновыми

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,15 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var navWrapper = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+navWrapper.classList.remove('main-nav--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+navToggle.addEventListener('click', function () {
+  if (navWrapper.classList.contains('main-nav--closed')) {
+    navWrapper.classList.remove('main-nav--closed');
+    navWrapper.classList.add('main-nav--opened');
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    navWrapper.classList.add('page-header--closed');
+    navWrapper.classList.remove('page-header--opened');
   }
 });
