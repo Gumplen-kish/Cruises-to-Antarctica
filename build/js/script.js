653202aa23ecf0b56c52d1b6da9a494ba3695b0b
Gumplen-kish <gumplen117@gmail.com>
2020-10-05 23:11:38 +0300

    Сделано мобильное меню, кроме no-js

--- build/js/script.js
+++ build/js/script.js
@@ -1,6 +1,10 @@
 'use strict';
 var navWrapper = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var pageWrapper = document.querySelector('.page-header__wrapper');
+var pageCenter = document.querySelector('.page-header__center');
+var pageLogo = document.querySelector('.page-header__logo');
+var pagePreview = document.querySelector('.page-header__preview');
 
 navWrapper.classList.remove('main-nav--nojs');
 
@@ -8,8 +12,16 @@ navToggle.addEventListener('click', function () {
   if (navWrapper.classList.contains('main-nav--closed')) {
     navWrapper.classList.remove('main-nav--closed');
     navWrapper.classList.add('main-nav--opened');
+    pageWrapper.classList.add('page-header__wrapper--opened');
+    pageCenter.classList.add('page-header__center--opened');
+    pageLogo.classList.add('page-header__logo--opened');
+    pagePreview.classList.add('page-header__preview--opened');
   } else {
     navWrapper.classList.add('main-nav--closed');
     navWrapper.classList.remove('main-nav--opened');
+    pageWrapper.classList.remove('page-header__wrapper--opened');
+    pageCenter.classList.remove('page-header__center--opened');
+    pageLogo.classList.remove('page-header__logo--opened');
+    pagePreview.classList.remove('page-header__preview--opened');
   }
 });
--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,9 @@
 var navWrapper = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 var pageWrapper = document.querySelector('.page-header__wrapper');
+var pageCenter = document.querySelector('.page-header__center');
+var pageLogo = document.querySelector('.page-header__logo');
+var pagePreview = document.querySelector('.page-header__preview');
 
 navWrapper.classList.remove('main-nav--nojs');
 
@@ -10,9 +13,15 @@ navToggle.addEventListener('click', function () {
     navWrapper.classList.remove('main-nav--closed');
     navWrapper.classList.add('main-nav--opened');
     pageWrapper.classList.add('page-header__wrapper--opened');
+    pageCenter.classList.add('page-header__center--opened');
+    pageLogo.classList.add('page-header__logo--opened');
+    pagePreview.classList.add('page-header__preview--opened');
   } else {
     navWrapper.classList.add('main-nav--closed');
     navWrapper.classList.remove('main-nav--opened');
     pageWrapper.classList.remove('page-header__wrapper--opened');
+    pageCenter.classList.remove('page-header__center--opened');
+    pageLogo.classList.remove('page-header__logo--opened');
+    pagePreview.classList.remove('page-header__preview--opened');
   }
 });
